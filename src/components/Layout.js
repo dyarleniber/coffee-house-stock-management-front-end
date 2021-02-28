fbf2d41dc56750ab00801b394ff443bf0373fc94
Dyarlen Iber <dyarlen1@gmail.com>
2021-02-28 01:14:09 +0000

    Add layout

---
+++ src/components/Layout.js
@@ -0,0 +1,92 @@
+import Link from "next/link";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import ArrowRight from "@material-ui/icons/ArrowRight";
+import Logo from "../components/Logo";
+import appConfig from "../config/app";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  logo: {
+    margin: theme.spacing(4),
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
+
+export default function Layout({ title, children }) {
+  const classes = useStyles();
+
+  const { menuItems } = appConfig;
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            {title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar}>
+          <div className={classes.logo}>
+            <Logo />
+          </div>
+        </div>
+        <Divider />
+        <List>
+          {menuItems.map((menu) => (
+            <Link href={menu.link} passHref key={menu.name}>
+              <ListItem button component="a">
+                <ListItemIcon>
+                  <ArrowRight />
+                </ListItemIcon>
+                <ListItemText primary={menu.name} />
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        {children}
+      </main>
+    </div>
+  );
+}
--- src/config/app.js
+++ src/config/app.js
@@ -1,5 +1,11 @@
 const config = {
   name: "Coffee House",
+  menuItems: [
+    { name: "Home", link: "/" },
+    { name: "Categories", link: "/categories" },
+    { name: "Products", link: "/products" },
+    { name: "Users", link: "/users" },
+  ],
 };
 
 export default config;
